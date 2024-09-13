9ca6b37366dcd1869d188fc89973761c9520cff9
Elroi Belete <elroibelete777@gmail.com>
2024-09-13 18:06:34 -0500

    Added constants and changed something with my .gitignore

--- src/Main.java
+++ src/Main.java
@@ -1,6 +1,23 @@
-//TIP To <b>Run</b> code, press <shortcut actionId="Run"/> or
-// click the <icon src="AllIcons.Actions.Execute"/> icon in the gutter.
+
 public class Main {
+
+    /* Character classes */
+    public static final int LETTER = 0;
+    public static final int DIGIT = 1;
+    public static final int UNKNOWN = 99;
+
+    /*Token Codes */
+    public static final int INT_LIT = 10;
+    public static final int IDENT = 11;
+    public static final int ASSIGN_OP = 20;
+    public static final int ADD_OP = 21;
+    public static final int SUB_OP = 22;
+    public static final int MULT_OP = 23;
+    public static final int DIV_OP = 24;
+    public static final int LEFT_PAREN = 25;
+    public static final int RIGHT_PAREN = 26;
+
+
     public static void main(String[] args) {
 
     }
